b4a079ea809a968b3fcec259b4d3e4874d8a7a7b
Hazur Rahman <hazur2708@gmail.com>
2026-05-01 01:59:26 +0530

    Use environment variable for server port

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const PORT = 5000;
+const PORT = process.env.PORT||5000;
 
 // ===============================
 // EMAIL TRANSPORTER
